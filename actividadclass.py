9214b9e060fcc824d78ca1c8b550b731e033233f
Agustinapelatti <115805834+Agustinapelatti@users.noreply.github.com>
2023-11-27 13:50:00 -0300

    Agustinapelatti

---
+++ actividadclass.py
@@ -0,0 +1,18 @@
+class Zahira:
+    def __init__(self,nombre,edad,apellido):
+        self.nombre =nombre
+        self.edad =edad
+        self.apellido =apellido
+
+    def presentacion(self):
+        print(f"hola soy {self.nombre}, mi apellido es {self.apellido} y tengo {self.edad} años.")
+
+class Agustina:
+    def __init__(self,nombre,edad,apellido):
+        self.nombre =nombre
+        self.edad =edad
+        self.apellido =apellido
+
+    def presentacion2(self):
+        print(f"hola soy {self.nombre}, mi apellido es {self.apellido} y tengo {self.edad} años.")
+    
